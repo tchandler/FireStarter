d2583bb2502c0d17b17b4039e3647978ecb12374
Thomas Chandler <zohogorganzola@gmail.com>
2011-12-03 23:42:42 -0500

    Fixed some JSLint errors Added call to trigger fire event

--- js/BlockStates.js
+++ js/BlockStates.js
@@ -12,7 +12,7 @@ EmptyBlockConfiguration = {
 	temperature: 0,
 	fullGrowth: 100,
 	growth: 0
-}
+};
 
 FlammableBlockConfiguration = {
 	temperature: 0,
@@ -24,7 +24,7 @@ FireBlockConfiguration = {
 	fireLife: 100,
 	fireDecay: 1.01,
 	fireTemperature: 100
-}
+};
 
 var BSM = {}; //Manage state(update)!
 var BDM = {}; //Manage drawing!
@@ -37,7 +37,7 @@ BSM = {
 	update: function() {
 		switch(this.state) {
 			case BlockStates.empty:
-				if(this.entryState) BSM.initEmptyBlock.call(this);;
+				if(this.entryState) BSM.initEmptyBlock.call(this);
 				BSM.updateEmptyBlock.call(this);
 				if(this.exitState) BSM.exitEmptyBlock.call(this);
 				break;
@@ -47,10 +47,10 @@ BSM = {
 				if(this.exitState) BSM.exitFlammableBlock.call(this);
 				break;
 			case BlockStates.fire:
-				if(this.entryState) BSM.initFireBlock.call(this);;
+				if(this.entryState) BSM.initFireBlock.call(this);
 				BSM.updateFireBlock.call(this);
 				if(this.exitState) BSM.exitFireBlock.call(this);
-				break;
+                break;
 			default:
 				break;
 		}
@@ -87,6 +87,7 @@ BSM = {
 			this.exitState = true;
 		}
 		//spread fire!
+        this.triggerFireEvent();
 		//?
 	},
 	//How do I shot init/exit better
@@ -151,7 +152,7 @@ BDM = {
 	draw: function(canvasContext) {
 		switch(this.state) {
 			case BlockStates.empty:
-				if(this.entryState) BSM.initEmptyBlock.call(this);;
+				if(this.entryState) BSM.initEmptyBlock.call(this);
 				BDM.drawEmptyBlock.call(this, canvasContext);
 				break;
 			case BlockStates.fire:
@@ -173,7 +174,7 @@ BDM = {
 		growthPercent = growthPercent.clamp(0, 1);
 		var colorGradient = canvasContext.createLinearGradient(this.x, this.y + this.drawDimension.height, this.x , this.y);
 		colorGradient.addColorStop(0, "green");
-		colorGradient.addColorStop(growthPercent * .9, "#00FF00");		
+		colorGradient.addColorStop(growthPercent * 0.9, "#00FF00");		
 		colorGradient.addColorStop(growthPercent, "#5C3317");
 		canvasContext.fillStyle = colorGradient;
 	},
@@ -183,9 +184,9 @@ BDM = {
 		var colorGradient = canvasContext.createLinearGradient(this.x, this.y  + this.drawDimension.height, this.x , this.y);
 		colorGradient.addColorStop(0, "red");
 		colorGradient.addColorStop(tempPercent, "orange");
-		if(tempPercent < .75) colorGradient.addColorStop(.25 + tempPercent, "yellow");
-		if(tempPercent < .5) colorGradient.addColorStop(.5 + tempPercent, "#8E6B23");
-		if(tempPercent < .25) colorGradient.addColorStop(.75 + tempPercent, "#5C3317");
+		if(tempPercent < 0.75) colorGradient.addColorStop(0.25 + tempPercent, "yellow");
+		if(tempPercent < 0.5) colorGradient.addColorStop(0.5 + tempPercent, "#8E6B23");
+		if(tempPercent < 0.25) colorGradient.addColorStop(0.75 + tempPercent, "#5C3317");
 		canvasContext.fillStyle = colorGradient;
 	},
 	drawFlammableBlock: function(canvasContext) {
@@ -196,4 +197,4 @@ BDM = {
 		colorGradient.addColorStop((tempPercent), "green");
 		canvasContext.fillStyle = colorGradient;
 	}
-}
+};
